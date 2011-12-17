564729e05bcfe010bb885cdbb7d56a862cb32675
Michael Carpenter <malcom2073@gmail.com>
2011-12-16 23:31:29 -0800

    Change from GPL to LGPLV2

--- ObdThread.cpp
+++ ObdThread.cpp
@@ -6,9 +6,9 @@
 *   This file is a part of libobd                                         *
 *                                                                         *
 *   libobd is free software: you can redistribute it and/or modify        *
-*   it under the terms of the GNU General Public License as published by  *
-*   the Free Software Foundation, either version 2 of the License, or     *
-*   (at your option) any later version.                                   *
+*   it under the terms of the GNU Lesser General Public License as        *
+*   published by the Free Software Foundation, either version 2 of        *
+*   the License, or (at your option) any later version.                   *
 *                                                                         *
 *   libobd is distributed in the hope that it will be useful,             *
 *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
